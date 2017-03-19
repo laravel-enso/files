f60fcee07f8d0360989bf0505ad704db04a8d9b0
aocneanu <aocneanu@gmail.com>
2017-03-19 16:47:04 +0000

    Apply fixes from StyleCI

--- src/FileManagerStatus.php
+++ src/FileManagerStatus.php
@@ -6,13 +6,12 @@ use LaravelEnso\Helpers\Classes\AbstractObject;
 
 class FileManagerStatus extends AbstractObject
 {
-	public $level;
-	public $message;
-	public $errors;
+    public $level;
+    public $message;
+    public $errors;
 
-	public function __construct()
-	{
-		$this->errors = collect();
-	}
+    public function __construct()
+    {
+        $this->errors = collect();
+    }
 }
-
