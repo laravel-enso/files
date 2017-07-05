d564e024d071437f2f52dcd38b3a4e07a61a34da
Adrian Ocneanu <aocneanu@gmail.com>
2017-07-05 19:48:21 +0000

    Apply fixes from StyleCI

--- src/Classes/FileUploader.php
+++ src/Classes/FileUploader.php
@@ -31,8 +31,8 @@ class FileUploader
     public function commit()
     {
         $this->files->each(function ($file) {
-            \Storage::disk($this->disk)->move(config('laravel-enso.paths.temp') . DIRECTORY_SEPARATOR . $file['saved_name'],
-                $this->filesPath . DIRECTORY_SEPARATOR . $file['saved_name']);
+            \Storage::disk($this->disk)->move(config('laravel-enso.paths.temp').DIRECTORY_SEPARATOR.$file['saved_name'],
+                $this->filesPath.DIRECTORY_SEPARATOR.$file['saved_name']);
         });
     }
 
@@ -52,22 +52,22 @@ class FileUploader
     {
         $fileName = $file->getClientOriginalName();
         $savedName = $file->hashName();
-        $tempPath = storage_path('app' . DIRECTORY_SEPARATOR . $this->tempPath);
+        $tempPath = storage_path('app'.DIRECTORY_SEPARATOR.$this->tempPath);
         $file->move($tempPath, $savedName);
-        $fileSize = \File::size($tempPath . DIRECTORY_SEPARATOR . $savedName);
+        $fileSize = \File::size($tempPath.DIRECTORY_SEPARATOR.$savedName);
 
         $this->files->push([
             'original_name' => $fileName,
-            'saved_name' => $savedName,
-            'size' => $fileSize,
-            'full_path' => $tempPath . DIRECTORY_SEPARATOR . $savedName,
+            'saved_name'    => $savedName,
+            'size'          => $fileSize,
+            'full_path'     => $tempPath.DIRECTORY_SEPARATOR.$savedName,
         ]);
     }
 
     public function deleteTempFiles()
     {
         $this->files->each(function ($file) {
-            $fileWithPath = config('laravel-enso.paths.temp') . DIRECTORY_SEPARATOR . $file['saved_name'];
+            $fileWithPath = config('laravel-enso.paths.temp').DIRECTORY_SEPARATOR.$file['saved_name'];
 
             return \Storage::has($fileWithPath) ? \Storage::disk($this->disk)->delete($fileWithPath) : null;
         });
@@ -83,7 +83,7 @@ class FileUploader
     {
         if (!$file->isValid()) {
             $this->deleteTempFiles();
-            throw new \EnsoException('Error Processing File:' . $file->getClientOriginalName(), 'error', [], 409);
+            throw new \EnsoException('Error Processing File:'.$file->getClientOriginalName(), 'error', [], 409);
         }
     }
 
@@ -96,7 +96,7 @@ class FileUploader
         if (!$this->extensionIsValid($file)) {
             $this->deleteTempFiles();
             throw new \EnsoException(
-                __('Allowed extensions').": ".implode(', ', $this->validExtensions), 'error', [], 409
+                __('Allowed extensions').': '.implode(', ', $this->validExtensions), 'error', [], 409
             );
         }
     }
