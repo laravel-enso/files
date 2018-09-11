b8955af8df1c4868f42e17391eec4806f2b38cea
mishu <mishu@xtelecom.ro>
2018-09-11 13:43:09 +0300

    makes all permissions default

--- src/database/migrations/2018_08_25_101000_create_structure_for_files.php
+++ src/database/migrations/2018_08_25_101000_create_structure_for_files.php
@@ -9,11 +9,11 @@ class CreateStructureForFiles extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.files.index', 'description' => 'List files', 'type' => 0, 'is_default' => false],
-        ['name' => 'core.files.link', 'description' => 'Get file download temporary link', 'type' => 0, 'is_default' => false],
-        ['name' => 'core.files.show', 'description' => 'Open file in browser', 'type' => 0, 'is_default' => false],
-        ['name' => 'core.files.download', 'description' => 'Download file', 'type' => 0, 'is_default' => false],
-        ['name' => 'core.files.destroy', 'description' => 'Delete file', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.files.index', 'description' => 'List files', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.files.link', 'description' => 'Get file download temporary link', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.files.show', 'description' => 'Open file in browser', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.files.download', 'description' => 'Download file', 'type' => 0, 'is_default' => true],
+        ['name' => 'core.files.destroy', 'description' => 'Delete file', 'type' => 1, 'is_default' => true],
     ];
 
     protected $menu = [
