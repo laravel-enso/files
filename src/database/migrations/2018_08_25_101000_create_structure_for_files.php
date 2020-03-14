b1243cdb38ceeee21a47373774c41039676c7f29
mishu <mishu@xtelecom.ro>
2020-03-14 18:28:28 +0200

    removes Types from migrations

--- src/database/migrations/2018_08_25_101000_create_structure_for_files.php
+++ src/database/migrations/2018_08_25_101000_create_structure_for_files.php
@@ -1,16 +1,15 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForFiles extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.files.index', 'description' => 'List files', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.files.link', 'description' => 'Get file download temporary link', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.files.show', 'description' => 'Open file in browser', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.files.download', 'description' => 'Download file', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'core.files.destroy', 'description' => 'Delete file', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.files.index', 'description' => 'List files', 'is_default' => true],
+        ['name' => 'core.files.link', 'description' => 'Get file download temporary link', 'is_default' => true],
+        ['name' => 'core.files.show', 'description' => 'Open file in browser', 'is_default' => true],
+        ['name' => 'core.files.download', 'description' => 'Download file', 'is_default' => true],
+        ['name' => 'core.files.destroy', 'description' => 'Delete file', 'is_default' => true],
     ];
 
     protected $menu = [
--- src/database/migrations/2018_08_25_103000_create_structure_for_uploads.php
+++ src/database/migrations/2018_08_25_103000_create_structure_for_uploads.php
@@ -1,12 +1,11 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForUploads extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.uploads.store', 'description' => 'Upload file', 'type' => Types::Write, 'is_default' => true],
-        ['name' => 'core.uploads.destroy', 'description' => 'Delete upload', 'type' => Types::Write, 'is_default' => true],
+        ['name' => 'core.uploads.store', 'description' => 'Upload file', 'is_default' => true],
+        ['name' => 'core.uploads.destroy', 'description' => 'Delete upload', 'is_default' => true],
     ];
 }
