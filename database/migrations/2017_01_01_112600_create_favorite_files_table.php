5a691ae96e32deb21801a0b297a887880abd4a22
mishu <mishu@xtelecom.ro>
2022-03-06 21:34:20 +0200

    adds created_at index

--- database/migrations/2017_01_01_112600_create_favorite_files_table.php
+++ database/migrations/2017_01_01_112600_create_favorite_files_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('favorite_files', function (Blueprint $table) {
@@ -21,6 +20,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->unique(['user_id', 'file_id']);
+            $table->index(['created_at']);
         });
     }
 
---
+++ src/Upgrades/CreatedAtIndex.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LaravelEnso\Files\Upgrades;
+
+use Illuminate\Support\Facades\Schema;
+use LaravelEnso\Upgrade\Contracts\BeforeMigration;
+use LaravelEnso\Upgrade\Contracts\MigratesTable;
+use LaravelEnso\Upgrade\Helpers\Table;
+
+class CreatedAtIndex implements MigratesTable, BeforeMigration
+{
+    public function isMigrated(): bool
+    {
+        return Table::hasIndex('files', 'files_created_at_index');
+    }
+
+    public function migrateTable(): void
+    {
+        Schema::table('files', fn ($table) => $table->index(['created_at']));
+    }
+}
