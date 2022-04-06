70dc69f5e21b8ab087f327c83cc626981d3aaf62
mishu <mishu@xtelecom.ro>
2022-04-06 16:06:40 +0300

    adds minssing unique() on file_id

--- database/migrations/2017_01_01_112400_create_uploads_table.php
+++ database/migrations/2017_01_01_112400_create_uploads_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('uploads', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->unsignedBigInteger('file_id')->nullable();
+            $table->unsignedBigInteger('file_id')->nullable()->unique();
             $table->foreign('file_id')->references('id')->on('files')
                 ->onUpdate('restrict')->onDelete('restrict');
 
