d79f5d5cd6a5088c2f25dc057eef13361d554904
Mihai <mihai.ocneanu@gmail.com>
2022-04-29 15:26:57 +0300

    fixed upgrade order & type (manual)

--- src/Upgrades/AddTypeIdCreatedAtIndex.php
+++ src/Upgrades/AddTypeIdCreatedAtIndex.php
@@ -4,11 +4,18 @@ namespace LaravelEnso\Files\Upgrades;
 
 use Illuminate\Support\Facades\Schema;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
+use LaravelEnso\Upgrade\Contracts\Prioritization;
+use LaravelEnso\Upgrade\Contracts\ShouldRunManually;
 use LaravelEnso\Upgrade\Helpers\Table;
 
-class AddTypeIdCreatedAtIndex implements MigratesTable
+class AddTypeIdCreatedAtIndex implements MigratesTable, Prioritization, ShouldRunManually
 {
     private const Index = 'files_type_id_created_at_index';
+
+    public function priority(): int
+    {
+        return 106;
+    }
 
     public function isMigrated(): bool
     {
