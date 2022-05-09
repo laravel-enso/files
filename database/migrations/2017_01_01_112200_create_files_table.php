fee2f5a66d36b49da9f14456525ba83249852d4f
mishu <mishu@xtelecom.ro>
2022-05-09 16:30:35 +0300

    adds missing index

--- database/migrations/2017_01_01_112200_create_files_table.php
+++ database/migrations/2017_01_01_112200_create_files_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('files', function (Blueprint $table) {
@@ -27,6 +26,8 @@ return new class extends Migration
             $table->foreign('created_by')->references('id')->on('users');
 
             $table->timestamps();
+
+            $table->index('created_at');
         });
     }
 
